cb469602e67b96d829c2989e4225e23ff9cd0fa0
Artemalo laptop <125931008+artemalo@users.noreply.github.com>
2024-11-05 00:03:39 +0300

    activity_register done

--- app/src/main/java/com/example/walkofinterest/SplashScreenActivity.java
+++ app/src/main/java/com/example/walkofinterest/SplashScreenActivity.java
@@ -16,7 +16,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+                startActivity(new Intent(SplashScreenActivity.this, RegisterActivity.class));
                 finish();
             }
 
