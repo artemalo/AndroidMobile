1416c3f67a769f26dd2c492b5a68c65e6e449eec
Artemalo laptop <125931008+artemalo@users.noreply.github.com>
2024-11-09 23:57:12 +0300

    activity_main - add numPicker Add func onClick "touchTime" = visible

--- app/src/main/java/com/example/walkofinterest/MainActivity.java
+++ app/src/main/java/com/example/walkofinterest/MainActivity.java
@@ -1,81 +1,43 @@
 package com.example.walkofinterest;
 
-import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.yandex.mapkit.Animation;
-import com.yandex.mapkit.MapKit;
-import com.yandex.mapkit.MapKitFactory;
-import com.yandex.mapkit.geometry.Point;
-import com.yandex.mapkit.map.CameraPosition;
-import com.yandex.mapkit.mapview.MapView;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.widget.Toast;
-
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int PERMISSION_REQUEST_CODE = 100;
-
-    private MapView mapView;
+    private Boolean isPickerNotVisible = true;
 
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        /*if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
-        }*/
-
-        /*MapKitFactory.setApiKey("28da8ea7-c995-4793-b13b-2971cf44e2f0");
-        MapKitFactory.initialize(this);*/
-
         setContentView(R.layout.activity_main);
-        /*mapView = (MapView)findViewById(R.id.mapview);
-        mapView.getMap().move(
-                new CameraPosition(new Point(47.202198, 38.935190), 18.0f, 0.0f, 0.0f),
-                new Animation(Animation.Type.SMOOTH, 0),
-                null);*/
-    }
 
-/*    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        ConstraintLayout touchTime = findViewById(R.id.touchTime);
+        NumberPicker numPickerTime = findViewById(R.id.numPickerTime);
+        LinearLayout layoutNumPickerTime = findViewById(R.id.layoutNumPickerTime);
 
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Да", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(this, "Нет", Toast.LENGTH_LONG).show();
+        numPickerTime.setMinValue(0);
+        numPickerTime.setMaxValue(12);
+
+        //TextView textTime = findViewById(R.id.textTime);
+
+        touchTime.setOnClickListener(v -> {
+            if (isPickerNotVisible)
+                layoutNumPickerTime.setVisibility(View.VISIBLE);
+            else {
+                //textTime.setText(numPickerTime.getValue()); CRASH IN THIS
+                layoutNumPickerTime.setVisibility(View.GONE);
             }
-        }
+            isPickerNotVisible = !isPickerNotVisible;
+        });
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        MapKitFactory.getInstance().onStart();
-        mapView.onStart();
-    }
 
-    @Override
-    protected void onStop() {
-        mapView.onStop();
-        MapKitFactory.getInstance().onStop();
-        super.onStop();
-    }*/
+
 }
