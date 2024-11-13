7c38699100fbed833681fa1c524026e34c4920f1
Artemalo laptop <125931008+artemalo@users.noreply.github.com>
2024-11-13 18:26:51 +0300

    StartRoute - working

--- app/src/main/java/com/example/walkofinterest/StartRouteActivity.java
+++ app/src/main/java/com/example/walkofinterest/StartRouteActivity.java
@@ -1,6 +1,8 @@
 package com.example.walkofinterest;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class StartRouteActivity extends AppCompatActivity {
+public class StartRouteActivity extends BaseButtons{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_start_route);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ImageButton btnProfile = findViewById(R.id.btnProfile);
+        if (btnProfile != null)
+            btnProfile.setOnClickListener(v -> ButtonProfile());
+
+        ImageButton btnBack = findViewById(R.id.btnBack);
+        if (btnBack != null)
+            btnBack.setOnClickListener(v -> ButtonBack(getBackActivityClass()));
+    }
+
+    @Override
+    protected Class<?> getBackActivityClass() {
+        return CategoriesActivity.class;
+    }
+
+    @Override
+    protected Class<?> getNextActivityClass() {
+        return null;//GoActivity
     }
 }
