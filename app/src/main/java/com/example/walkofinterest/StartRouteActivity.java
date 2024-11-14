199a90dfadc05ec1c6c50f7f13f2ecba864c80ee
Artemalo laptop <125931008+artemalo@users.noreply.github.com>
2024-11-14 18:20:47 +0300

    StartRoute - 95/100 +Change from static to dynamic bottomSheet on activity +Correct setOnTouchListener for RVRouteInformation

--- app/src/main/java/com/example/walkofinterest/StartRouteActivity.java
+++ app/src/main/java/com/example/walkofinterest/StartRouteActivity.java
@@ -3,13 +3,15 @@ package com.example.walkofinterest;
 import android.annotation.SuppressLint;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.widget.ImageButton;
+import android.view.MotionEvent;
+import android.view.View;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.walkofinterest.models.adapters.RouteInfoModel;
 import com.example.walkofinterest.models.adapters.RouteInfoRVAdapter;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import java.util.ArrayList;
 
@@ -21,15 +23,43 @@ public class StartRouteActivity extends BaseButtons{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_route);
 
+        SetBottomSheet();
+
+        findViewById(R.id.btnProfile).setOnClickListener(v -> ButtonProfile());
+        findViewById(R.id.btnBack).setOnClickListener(v -> ButtonBack(getBackActivityClass()));
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void SetBottomSheet() {
         SetRouteInformation();
 
-        ImageButton btnProfile = findViewById(R.id.btnProfile);
-        if (btnProfile != null)
-            btnProfile.setOnClickListener(v -> ButtonProfile());
+        View bottomSheet = findViewById(R.id.bottom_sheet_routes);
+        BottomSheetBehavior<View> bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
 
-        ImageButton btnBack = findViewById(R.id.btnBack);
-        if (btnBack != null)
-            btnBack.setOnClickListener(v -> ButtonBack(getBackActivityClass()));
+        findViewById(R.id.recyclerViewRouteInformation).setOnTouchListener((v, event) -> {
+            switch (event.getAction())
+            {
+                case MotionEvent.ACTION_DOWN:
+                case MotionEvent.ACTION_MOVE:
+                    bottomSheetBehavior.setDraggable(false);
+                    break;
+
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    bottomSheetBehavior.setDraggable(true);
+                    break;
+            }
+            return false;
+        });
+
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+        bottomSheetBehavior.setPeekHeight(400);
+
+        int screenHeight = getResources().getDisplayMetrics().heightPixels;
+        int margin80dp = (int) (80 * getResources().getDisplayMetrics().density);
+
+        bottomSheetBehavior.setMaxHeight(screenHeight - margin80dp);
     }
 
     @Override
@@ -53,31 +83,31 @@ public class StartRouteActivity extends BaseButtons{
 
     @SuppressLint("UseCompatLoadingForDrawables")
     private void SetUpRouteInfoModels() {
-        //int[] indexArray = {1, 2};
-        final int n = 3;
+        final int n = 30;
         int[] timeArray = new int[n];
-        int[] countSteps = new int [n];
+        int[] countSteps = new int[n];
 
         for (int i = 0; i < n; i++) {
             timeArray[i] = 60 * (i + 1);
             countSteps[i] = 10000 * (i + 1);
         }
 
-        Drawable drawable;
-        int i = 0;
-        {
-            drawable = getDrawable(R.drawable.route_green);
-            routeInfoModels.add(new RouteInfoModel(i+1, timeArray[i], countSteps[i], drawable));
-            i++;
-        }
-        {
-            drawable = getDrawable(R.drawable.route_yellow);
-            routeInfoModels.add(new RouteInfoModel(i+1, timeArray[i], countSteps[i], drawable));
-            i++;
-        }
-        {
-            drawable = getDrawable(R.drawable.route_red);
-            routeInfoModels.add(new RouteInfoModel(i+1, timeArray[i], countSteps[i], drawable));
+        for (int i = 0; i < n; i++) {
+            Drawable drawable;
+            {
+                drawable = getDrawable(R.drawable.route_green);
+                routeInfoModels.add(new RouteInfoModel(i + 1, timeArray[i], countSteps[i], drawable));
+                i++;
+            }
+            {
+                drawable = getDrawable(R.drawable.route_yellow);
+                routeInfoModels.add(new RouteInfoModel(i + 1, timeArray[i], countSteps[i], drawable));
+                i++;
+            }
+            {
+                drawable = getDrawable(R.drawable.route_red);
+                routeInfoModels.add(new RouteInfoModel(i + 1, timeArray[i], countSteps[i], drawable));
+            }
         }
     }
 }
